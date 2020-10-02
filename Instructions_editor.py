4a8fa595e04ade88042e66c803c4c9bfc55e3213
Charles <66975487+witlesswitness@users.noreply.github.com>
2020-10-02 11:45:48 -0400

    tidied up. removed debugging

--- Instructions_editor.py
+++ Instructions_editor.py
@@ -4,33 +4,23 @@ import os
 
 def help_me():
     dothis = "echo 'Use Direditor as such: first argument is the command to execute: clear, append, or replace. Append and replace must have two additional arguments: bots to instruct, followed by commands to execute'"
-    # return(dothis)
     os.system(dothis)
 
 def clear():
     dothis = "echo -n '' > ./server/instructions.txt"
-    # return(dothis)
     os.system(dothis)
     
-    
-
 def replace_commands(s1,s2):
     bots = s1.strip().split(',')
     commands = s2.strip().split(',')
-    # print(commands)
     instructions = []
     for item in bots:
-        # print(item)
         for command in commands:
             addon = ">> sendthis.txt"
-            # print(commands[counter])
             instructions.append(f'{item} {command} {addon}')
     directions = ('\n'.join(instructions))
     dothis = "echo " + "'" + directions + "'" + " > ./server/instructions.txt"
-    # return(dothis)
     os.system(dothis)
-    # return(directions)
-    # return('\n'.join(instructions))
     
 def append_commands(s1,s2):
     bots = s1.strip().split(',')
@@ -38,26 +28,17 @@ def append_commands(s1,s2):
     # print(commands)
     instructions = []
     for item in bots:
-        # print(item)
         for command in commands:
             addon = ">> sendthis.txt"
-            # print(commands[counter])
             instructions.append(f"{item} {command} {addon}")
     directions = ('\n'.join(instructions))
     dothis = "echo " + "'" + directions + "'" + " >> ./server/instructions.txt"
-    # return(dothis)
     os.system(dothis)
-    
-    # return(directions)
-
-    # return('\n'.join(instructions))
 
 def main(s1,s2,s3):
     do_what = str(s1)
     bots = str(s2)
-    # print(bots)
     commands = str(s3)
-    # print(commands)
     if do_what == 'replace':
         replace_commands(bots, commands)
     elif do_what == 'append':
@@ -74,8 +55,6 @@ if __name__ == '__main__':
         commands = 0
     else:
         bots = sys.argv[2]
-        # print(bots)
         commands = sys.argv[3]
-        # print(commands)
 
     main(do_what, bots, commands)
